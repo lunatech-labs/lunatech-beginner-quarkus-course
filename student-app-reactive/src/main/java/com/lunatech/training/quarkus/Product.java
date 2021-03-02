fee96c1d0e3a83184efbaf57e5085477f0249367
Erik Bakker <erik@eamelink.net>
2021-03-02 21:33:56 +0100

    PUT endpoint with validation

--- student-app-reactive/src/main/java/com/lunatech/training/quarkus/Product.java
+++ student-app-reactive/src/main/java/com/lunatech/training/quarkus/Product.java
@@ -2,15 +2,24 @@ package com.lunatech.training.quarkus;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import io.vertx.mutiny.sqlclient.Row;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import java.math.BigDecimal;
 
 @Entity
 public class Product extends PanacheEntity {
 
+    @Length(min = 3)
     public String name;
+
+    @Length(min = 10)
     public String description;
+
+    @DecimalMin(value = "0.0", inclusive = true)
+    @Digits(integer = 10, fraction = 2)
     public BigDecimal price;
 
     public Product() {
--- student-app-reactive/src/main/java/com/lunatech/training/quarkus/ProductsResource.java
+++ student-app-reactive/src/main/java/com/lunatech/training/quarkus/ProductsResource.java
@@ -7,6 +7,9 @@ import io.vertx.mutiny.sqlclient.RowSet;
 import io.vertx.mutiny.sqlclient.Tuple;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -26,10 +29,29 @@ public class ProductsResource {
         return Product.findById(id);
     }
 
+    @Transactional
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<Product> update(@PathParam("id") Long id, @Valid @Named("product") Product product) {
+        return Product.<Product>findById(id).flatMap(p -> {
+            if(p == null) {
+              return Uni.createFrom().failure(new NotFoundException());
+            } else {
+                p.name = product.name;
+                p.description = product.description;
+                p.price = product.price;
+                return p.persistAndFlush().map(__ -> p);
+            }
+        });
+    }
+
     @GET
     @Path("search/{term}")
     public Uni<List<Product>> products(@PathParam("term") String term) {
         return Product.find("description like ?1 or name like ?1", "%" + term + "%").list();
     }
+
+
 
 }
